7a5b783bb4b4787598a0568c8ce37b4878b51dd2
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-11-14 22:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let line = document.getElementById("line")
+	let x = 0;
+let y = setInterval(function() {
+	line.style.transform=`rotate(${x}deg)`
+	x++;
+}, 200);
